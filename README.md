32be7e3a34c9cee0897695db0734f5cfe505a52f
htkael <htkael@gmail.com>
2025-01-06 13:04:40 -0600

    D

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Image-Carousel
+
+# Usage
+
+document.addEventListener("DOMContentLoaded", () => {
+const images = [
+"https://picsum.photos/200/300?random=1",
+"https://picsum.photos/200/300?random=2",
+"https://picsum.photos/200/300?random=3",
+"https://picsum.photos/200/300?random=4",
+];
+
+const image = new Carousel(images);
+console.log(image);
+const body = document.querySelector("body");
+image.appendTo(body);
+});
--- src/index.js
+++ src/index.js
@@ -103,17 +103,3 @@ export class Carousel {
     parent.append(this.slideShowElement);
   }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  const images = [
-    "https://picsum.photos/200/300?random=1",
-    "https://picsum.photos/200/300?random=2",
-    "https://picsum.photos/200/300?random=3",
-    "https://picsum.photos/200/300?random=4",
-  ];
-
-  const image = new Carousel(images);
-  console.log(image);
-  const body = document.querySelector("body");
-  image.appendTo(body);
-});
